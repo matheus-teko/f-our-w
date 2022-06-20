aba6ae8ae16d62c607a9ef159d053c3e43b2dcae
matheus-teko <matheus.work94@gmail.com>
2022-06-20 20:09:17 -0300

    f our w

---
+++ script.js
@@ -0,0 +1,18 @@
+for (let num = 1; num <= 10; num++){
+    console.log(num);
+  }
+  let num = 10;
+while (num >= 1){
+  console.log(num);
+  num--;
+}
+for(let i = 1; i <= 100; i++){
+    if(i % 2 !== 0){
+        console.log("Números ímpares:", i);
+    }
+}
+for(let num = 0; num <= 100; num++){
+    if(num % 2 ===0){
+    console.log(`Números pares ${num}`);
+}
+}
